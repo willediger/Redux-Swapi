a8c3802b6ba3d1686a497f93e7741ccb9d178aa6
unknown <will.ediger@gmail.com>
2019-04-16 02:27:44 -0500

    added fetching action types

---
+++ src/actions/actionTypes.js
@@ -0,0 +1,5 @@
+// we'll need to create 3 different action types here.
+// one for fetching, one for success and one for failure
+export const FETCHING_CHARACTERS = "FETCHING_CHARACTERS";
+export const FETCHING_SUCCESS = "FETCHING_SUCCESS";
+export const FETCHING_FAILURE = "FETCHING_FAILURE";
